7c4e0afcff91ad5b01b37441f6d91cb9c5c53e97
Bautista Bambozzi <bautistabambozzi@gmail.com>
2023-09-30 18:58:14 -0300

    slate clean

--- src/main/java/org/miniconvo/Main.java
+++ src/main/java/org/miniconvo/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Commencing program..");
+    public static void main(String[] args) {
     }
 }
--- src/main/java/org/miniconvo/server/Server.java
+++ src/main/java/org/miniconvo/server/Server.java
@@ -1,49 +1,14 @@
 package org.miniconvo.server;
 
-import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Objects;
 
-public class Server {
-
-    private final ServerSocket serverSocket;
-
-    Server(ServerSocket serverSocket) {
-        this.serverSocket = serverSocket;
-    }
-
-    private void closeSocket(Socket socket) {
-        if (!Objects.isNull(socket)) {
-            try {
-                socket.close();
-            } catch (IOException ignored) {
-            }
-        }
-    }
+public record Server(ServerSocket serverSocket) {
 
     public void start() {
-        try {
-            while (!serverSocket.isClosed()) {
-                // do work
-                Socket acceptedSocket = serverSocket.accept(); // blocks
-                System.out.println("New client connected!");
-                ClientHandler clientHandler = new ClientHandler(acceptedSocket);
-                Thread clientHandlerThread = new Thread(clientHandler);
-                clientHandlerThread.start();
-            }
-        } catch (IOException e) {
-            // TODO Remove printing the entire stack trace
-            e.printStackTrace();
+        while (!serverSocket.isClosed()) {
+            Socket connectedClient = serverSocket.accept();
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(1337);
-        Server server = new Server(serverSocket);
-        System.out.println("Starting server..");
-        server.start();
-        System.out.println("Started server!");
-    }
 }
-
