aba6338214b83aba011a6697c9e396af88767d05
Bautista Bambozzi <bautistabambozzi@gmail.com>
2023-09-30 20:35:00 -0300

    Added some client logic

--- src/main/java/org/miniconvo/client/Client.java
+++ src/main/java/org/miniconvo/client/Client.java
@@ -1,10 +1,8 @@
 package org.miniconvo.client;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.Socket;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Client {
@@ -12,12 +10,14 @@ public class Client {
     private BufferedWriter writer;
     private BufferedReader reader;
     private String username;
+    private Socket socket;
 
     Client(Socket socket) {
         try {
             this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            this.username = askForUsernameViaCli();
+            this.socket = socket;
+            // this.username = askForUsernameViaCli(); TODO? Ask server?
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -29,5 +29,37 @@ public class Client {
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
         return userInput;
-    };
+    }
+
+
+    private void closeAll() {
+        try {
+            if (Objects.nonNull(writer)) { writer.close();}
+            if (Objects.nonNull(reader)) { reader.close();}
+            if (Objects.nonNull(socket)) { socket.close();}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void listen() {
+        System.out.println("Started listening..");
+        while (this.socket.isConnected()) {
+            try {
+            System.out.println(reader.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+                closeAll();
+            }
+        }
+        System.out.println("Finished listening..");
+    }
+
+    public static void main(String[] args) throws IOException {
+        Socket clientSocketConnection = new Socket("localhost", 1337);
+        Client client = new Client(clientSocketConnection);
+        System.out.println("Connected.");
+        client.listen();
+    }
+
 }
--- src/main/java/org/miniconvo/server/ClientHandler.java
+++ src/main/java/org/miniconvo/server/ClientHandler.java
@@ -1,14 +1,28 @@
 package org.miniconvo.server;
 
+import java.io.*;
 import java.net.Socket;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class ClientHandler {
 
-    private final String username;
+    private static final Set<ClientHandler> allClients = new HashSet<>();
+    private String username;
+    private BufferedWriter writer;
+    private BufferedReader reader;
+
 
     ClientHandler(Socket socket) {
-        this.username = askForUsernameFromConsole(); // todo change this
+        try {
+            this.username = askForUsernameFromConsole(); // todo change this
+            this.writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            broadcastMessage(this.username + " has joined the chat.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -18,6 +32,19 @@ public class ClientHandler {
         String userInput = scanner.nextLine();
         System.out.println("Received username: " + userInput);
         return userInput;
+    }
+
+    private void broadcastMessage(String messageToBroadcast) {
+        try {
+
+            for (ClientHandler clientHandler : allClients) {
+                clientHandler.writer.write(messageToBroadcast);
+                writer.newLine();
+                writer.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getUsername() {
