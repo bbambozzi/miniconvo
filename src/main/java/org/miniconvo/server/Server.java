0536eb11c489e31ae596dd1f2acd03a6b7e1e19b
Bautista Bambozzi <bautistabambozzi@gmail.com>
2023-09-30 14:22:11 -0300

    utilized clienthandler to handle incoming user requests

--- src/main/java/org/miniconvo/server/Server.java
+++ src/main/java/org/miniconvo/server/Server.java
@@ -3,19 +3,24 @@ package org.miniconvo.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
 
 public class Server {
 
-    ServerSocket serverSocket;
+    private final ServerSocket serverSocket;
+
+    Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
 
     public void start() {
         try {
             while (!serverSocket.isClosed()) {
                 // do work
-                Socket incomingSocket = serverSocket.accept();
-                System.out.println("New client connected");
-                // TODO add client handler
+                Socket acceptedSocket = serverSocket.accept(); // blocks
+                System.out.println("New client connected!");
+                ClientHandler clientHandler = new ClientHandler(acceptedSocket);
+                Thread clientHandlerThread = new Thread(clientHandler);
+                clientHandlerThread.start();
             }
         } catch (IOException e) {
             // TODO Remove printing the entire stack trace
