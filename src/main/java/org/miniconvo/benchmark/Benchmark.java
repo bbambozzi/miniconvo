399b72d1eb66cae072bec5a1aa7726fdd230e272
Bautista Bambozzi <bautistabambozzi@gmail.com>
2023-10-02 21:39:16 -0300

    Added some basic benchmarking for rough tests

--- src/main/java/org/miniconvo/benchmark/Benchmark.java
+++ src/main/java/org/miniconvo/benchmark/Benchmark.java
@@ -6,21 +6,18 @@ import java.util.List;
 
 public class Benchmark {
 
-    Exception e catch(
-
-    {
-        e.printStackTrace();
-    })
-
-public static void main(String[] args) throws IOException {
-        if (args.length < 1) {
+    public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
             throw new IllegalArgumentException("Please provide the path as argument.");
         }
         String path = args[0];
+        int threadsToSpawn = Integer.parseInt(args[1]);
         String[] processArgs = new String[]{"java", "-jar", path};
+        RandomizedClient randomizedClient = new RandomizedClient(processArgs);
+        System.out.println("Starting threads..");
+        for (int i = 0; i < threadsToSpawn; i++) {
+            Thread.ofVirtual().start(randomizedClient);
+        }
 
-        Process process = new ProcessBuilder().command(processArgs).start();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
     }
 }
