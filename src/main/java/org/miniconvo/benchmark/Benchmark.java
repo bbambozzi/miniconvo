97f7a33db5a2be06fb76d108cb1d249d7bf78a6a
Bautista Bambozzi <bautistabambozzi@gmail.com>
2023-10-02 22:10:26 -0300

    scrapped current benchmarks

--- src/main/java/org/miniconvo/benchmark/Benchmark.java
+++ src/main/java/org/miniconvo/benchmark/Benchmark.java
@@ -6,18 +6,4 @@ import java.util.List;
 
 public class Benchmark {
 
-    public static void main(String[] args) throws IOException {
-        if (args.length < 2) {
-            throw new IllegalArgumentException("Please provide the path as argument.");
-        }
-        String path = args[0];
-        int threadsToSpawn = Integer.parseInt(args[1]);
-        String[] processArgs = new String[]{"java", "-jar", path};
-        RandomizedClient randomizedClient = new RandomizedClient(processArgs);
-        System.out.println("Starting threads..");
-        for (int i = 0; i < threadsToSpawn; i++) {
-            Thread.ofVirtual().start(randomizedClient);
-        }
-
-    }
 }
